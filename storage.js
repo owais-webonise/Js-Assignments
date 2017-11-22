f8dc061ebb5bfb2a88291c38fd90a54c9d588867
owais-webonise <owais.kazi@weboniselab.com>
2017-11-22 15:14:51 +0530

    Change files - Change js file with parameter passing - Change HTML file with function calling

--- storage.js
+++ storage.js
@@ -1,4 +1,107 @@
+
+var session = {storage: sessionStorage};
+var local = {storage: localStorage};
 // SET
+var setData =function(){
+	var key_data= document.getElementById("key11").value;
+	var value_data=document.getElementById("key12").value;
+	if (document.getElementById("key11").value.length == 0 || document.getElementById("key12").value.length == 0 )
+    	{
+    			alert("Error Invalid Input");
+    	}
+    else
+    {	
+	this.storage.setItem(key_data,value_data);
+	alert("Added");
+	 }
+}
+var setInLS=setData.bind(local);
+var setInSS=setData.bind(session);
+
+//GET
+var getData = function(){
+	var get_key= document.getElementById("key21").value;
+	if (get_key == 0)
+    	{
+    			alert("Error Invalid Input");
+    	}
+    else
+    {	
+	console.log(this.storage.getItem(get_key));
+	alert("Received");
+	 }
+
+}
+
+var getLS=getData.bind(local);
+var getSS=getData.bind(session);
+
+//GETALL
+var getAllData = function(){
+	var str=this.storage.length;
+	if (str == 0)
+    	{
+    			alert("Empty");
+    	}
+    else
+    {
+    for (var i = 0; i < str; i++){	
+	 var key = this.storage.key(i);
+	 console.log(key);
+	//alert("Received");
+	}
+	}
+
+}
+
+var getAllLS=getAllData.bind(local);
+var getAllSS=getAllData.bind(session);
+
+
+var clearData= function(){
+	this.storage.clear();
+	alert("Deleted");
+}
+
+var clearLS=clearData.bind(local);
+var clearSS=clearData.bind(session);
+
+
+// Cookie 
+document.getElementById("buttons").addEventListener("click",function(evt) {
+	var target = evt.target;
+    if (target.id == 'button12'){
+		//////////////CALL/////////////////////
+		var obj= {name :document.getElementById("key11").value};
+		var print3 = function(b){
+			return document.cookie = obj.name + '=' + b;
+		};
+		console.log(print3.call(this, document.getElementById("key12").value));
+		alert("Added");
+	}
+},false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 document.getElementById("buttons").addEventListener("click",function(evt) {
     var target = evt.target;
     if (target.id == 'button1'){
@@ -94,3 +197,4 @@ document.getElementById("buttons3").addEventListener("click",function(evt3) {
 
 
 
+*/
